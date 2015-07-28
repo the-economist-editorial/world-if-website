e0bfb610ee5561678c78cb0bdc0b23e26d71b45c
Keith Cirkel <github@keithcirkel.co.uk>
2015-07-28 11:42:52 +0100

    Preload content into app through compiled JS

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import App from '@economist/component-world-if-app';
+import ArticleStore from '@economist/component-articlestore';
+import content from '@economist/world-if-assets';
+(new ArticleStore()).setContent(content);
 /* eslint-env browser */
 React.render(React.createElement(App, {
   path: window.location.pathname,
